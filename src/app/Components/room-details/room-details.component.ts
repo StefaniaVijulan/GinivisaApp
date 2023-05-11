3587996c50c4cc5b2224a2453b7c0ec435efafd2
Stefania <vijulans@yahoo.com>
2023-05-11 21:35:49 +0300

    add photo

--- src/app/Components/room-details/room-details.component.ts
+++ src/app/Components/room-details/room-details.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ViewEncapsulation, ViewChild } from "@angular/core";
+import { SwiperComponent } from "swiper/angular";
+
+// import Swiper core and required modules
+import SwiperCore, { FreeMode, Navigation, Thumbs } from "swiper";
+
+// install Swiper modules
+SwiperCore.use([FreeMode, Navigation, Thumbs]);
 
 @Component({
   selector: 'app-room-details',
   templateUrl: './room-details.component.html',
   styleUrls: ['./room-details.component.scss'],
-  inputs: ['room']
+  inputs: ['room'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class RoomDetailsComponent implements OnInit {
   public room: any;
-  
+  thumbsSwiper: any;
   constructor(private _activateRouter: ActivatedRoute) { }
 
   ngOnInit() {
